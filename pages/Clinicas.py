c3dd56fa6c040c7946f16a12bebfd839f0135f31
ClinicaJair <drjairjaguaquara@gmail.com>
2026-05-09 19:51:43 -0300

    Add files via upload

--- pages/Clinicas.py
+++ pages/Clinicas.py
@@ -28,7 +28,7 @@ with st.form("form_clinica"):
     # 1. Definir proporções (ex: 20% / 80%)
     col1, col2, col3, col4 = st.columns([1,3,3,1])
     col5, col6 = st.columns([5,3])
-    col7 = st.columns()
+    col7 = st.columns([1])
     col8, col9, col10, col11 = st.columns([2,4,4,1])
 
     # 2. Usar 'with' para adicionar widgets nas colunas
