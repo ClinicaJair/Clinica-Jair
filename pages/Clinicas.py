d8c973144384d6f24a7a0e28bb149a4af41df21d
ClinicaJair <drjairjaguaquara@gmail.com>
2026-05-09 21:37:13 -0300

    Add files via upload

--- pages/Clinicas.py
+++ pages/Clinicas.py
@@ -107,7 +107,10 @@ with st.form("form_clinica"):
 
 if submit:
     # Inserir no PostgreSQL
-    supabase.table("clinicas").insert({"Razão Social": razao, "Nome Fantasia": fantasia,"Telefone": telefone }).execute()
+    supabase.table("clinicas").insert({"Codigo": codigo, "Razão Social": razao, "Nome Fantasia": fantasia, "Endereço": endereco,
+                                       "CEP": cep, "Bairro":bairro, "Cidade": cidade, "Estado": estado, "Telefone": telefone,
+                                       "Telefone1": telefone1, "CNPJ": cnpj, "Inscrição Estadual": inscricao,
+                                       "Data de Fundação": data_fundacao, "E-mail": email, "Site": site, "Stagram": stragram }).execute()
     st.success(f"Clinica {razao} cadastrado!")
 
 # Listar clientes
